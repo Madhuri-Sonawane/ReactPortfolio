603bba76555949a448f7810b148377d4b2db78aa
Madhuri Sonawane <madhurisonawane851@gmail.com>
2025-12-07 12:14:29 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import NavBar from "./components/Navbar"
+import FeatureCards from "./sections/FeatureCards"
+import Hero from "./sections/Hero"
+import ShowcaseSection from "./sections/ShowcaseSection"
+
+const App = () => {
+  return (
+    <>
+    <NavBar/>
+    <Hero/>
+    <ShowcaseSection/>
+    <FeatureCards/>
+    </>
+  )
+}
+
+export default App
+
