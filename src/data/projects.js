af48ae1d5af1b4135294927565cc2258f56c145d
Madhuri Sonawane <madhurisonawane851@gmail.com>
2026-02-15 11:09:29 +0530

    ProjectAdd

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,5 +1,6 @@
 import cineflix from "../assets/cineflix.png";
 import portfolio from "../assets/portfolio.png";
+import TestEnvironment from "../assets/TestEnvironment.png";
 
 export const projects = [
   {
@@ -12,7 +13,7 @@ export const projects = [
     ],
     tech: ["React", "JavaScript", "Tailwind CSS", "REST API"],
     live: "https://cine-flix-pi.vercel.app/",
-    code: "https://github.com/your-username/cineflix",
+    code: "https://github.com/Madhuri-Sonawane/cineflix",
   },
   {
     title: "Personal Portfolio Website",
@@ -24,6 +25,35 @@ export const projects = [
     ],
     tech: ["React", "Tailwind CSS", "GSAP", "Vite"],
     live: "https://react-portfolio-gold-delta.vercel.app/",
-    code: "https://github.com/your-username/portfolio",
+    code: "https://github.com/Madhuri-Sonawane/portfolio",
   },
+  {
+  title: "Secure Exam Environment (MERN Full Stack)",
+  image: TestEnvironment,
+  points: [
+    "Built secure proctored exam system with webcam + tab monitoring",
+    "Separate Employer & Candidate dashboards",
+    "Backend authentication with JWT and MongoDB Atlas"
+  ],
+  tech: [
+    "React",
+    "Node.js",
+    "MongoDB",
+    "Express",
+    "Tailwind CSS",
+    "JWT",
+    "MongoDB Atlas"
+  ],
+
+  live: [
+    { label: "Frontend", url: "https://online-exam-checker-six.vercel.app/" },
+    { label: "Backend API", url: "https://exam-secure-environment.onrender.com/" },
+    { label: "Employer Panel", url: "https://exam-secure-environment.onrender.com/employer/" }
+  ],
+
+  code: [
+    { label: "Frontend Repo", url: "https://github.com/Madhuri-Sonawane/online-exam-checker" },
+    { label: "Backend Repo", url: "https://github.com/Madhuri-Sonawane/exam-secure-environment" }
+  ]
+},
 ];
